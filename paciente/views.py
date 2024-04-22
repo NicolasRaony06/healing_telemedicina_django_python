57954b70aca9b84d659e0cc1e3936ed276c3b5c9
Nicolas Raony Dantas Queiroz <163556444+NicolasRaony06@users.noreply.github.com>
2024-04-21 23:34:50 -0300

    Add files via upload

---
+++ paciente/views.py
@@ -0,0 +1,54 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from medico.models import DadosMedico, Especialidades, DatasAbertas, is_medico
+from datetime import datetime, timedelta
+from .models import Consulta
+from django.contrib.messages import constants, add_message
+
+def home(request):
+    if request.method == 'GET':
+        medico_filtrar = request.GET.get('medico')
+        especialidades_filtrar = request.GET.getlist('especialidades')
+        medicos = DadosMedico.objects.all()
+
+        if medico_filtrar:
+            medicos = medicos.filter(nome__icontains=medico_filtrar)
+
+        if especialidades_filtrar:
+            medicos = medicos.filter(especialidade_id__in=especialidades_filtrar)
+
+        especialidades = Especialidades.objects.all()
+
+        return render(request, 'home.html', {'medicos': medicos, 'especialidades': especialidades})
+    
+def escolher_horario(request, id_dados_medicos):
+    if request.method == 'GET':
+        medico = DadosMedico.objects.get(id=id_dados_medicos)
+        datas_abertas = DatasAbertas.objects.filter(user=medico.user).filter(data__gte=datetime.now())
+
+        return render(request, 'escolher_horario.html', {'medico': medico, 'datas_abertas': datas_abertas})
+
+def agendar_horario(request, id_data_aberta):
+    if request.method == 'GET':
+        data_aberta = DatasAbertas.objects.get(id=id_data_aberta)
+        
+        horario_agendado = Consulta(
+            paciente = request.user,
+            data_aberta = data_aberta,
+
+        )
+
+        horario_agendado.save()
+
+        data_aberta.agendado = True
+        data_aberta.save()
+
+        add_message(request, constants.SUCCESS, 'Consulta agendada com sucesso')
+        return redirect('/pacientes/minhas_consultas')
+    
+def minhas_consultas(request):
+    if request.method == "GET":
+        #TODO: desenvolver filtros
+        minhas_consultas = Consulta.objects.filter(paciente=request.user).filter(data_aberta__data__gte=datetime.now())
+        return render(request, 'minhas_consultas.html', {'minhas_consultas': minhas_consultas})
+    
