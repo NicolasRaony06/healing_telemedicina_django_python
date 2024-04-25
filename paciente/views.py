4f0ab3c1f107ca66c8c6bfc914b38c400d18f348
Nicolas Raony Dantas Queiroz <163556444+NicolasRaony06@users.noreply.github.com>
2024-04-24 22:30:05 -0300

    Adicionando gerenciamento de consulta do paciente

--- paciente/views.py
+++ paciente/views.py
@@ -19,14 +19,14 @@ def home(request):
 
         especialidades = Especialidades.objects.all()
 
-        return render(request, 'home.html', {'medicos': medicos, 'especialidades': especialidades})
+        return render(request, 'home.html', {'medicos': medicos, 'especialidades': especialidades, 'is_medico': is_medico(request.user)})
     
 def escolher_horario(request, id_dados_medicos):
     if request.method == 'GET':
         medico = DadosMedico.objects.get(id=id_dados_medicos)
         datas_abertas = DatasAbertas.objects.filter(user=medico.user).filter(data__gte=datetime.now())
 
-        return render(request, 'escolher_horario.html', {'medico': medico, 'datas_abertas': datas_abertas})
+        return render(request, 'escolher_horario.html', {'medico': medico, 'datas_abertas': datas_abertas, 'is_medico': is_medico(request.user)})
 
 def agendar_horario(request, id_data_aberta):
     if request.method == 'GET':
@@ -50,5 +50,11 @@ def minhas_consultas(request):
     if request.method == "GET":
         #TODO: desenvolver filtros
         minhas_consultas = Consulta.objects.filter(paciente=request.user).filter(data_aberta__data__gte=datetime.now())
-        return render(request, 'minhas_consultas.html', {'minhas_consultas': minhas_consultas})
+        return render(request, 'minhas_consultas.html', {'minhas_consultas': minhas_consultas, 'is_medico': is_medico(request.user)})
     
+def consulta(request, id_consulta):
+    if request.method == 'GET':
+        consulta =  Consulta.objects.get(id=id_consulta)
+        dado_medico = DadosMedico.objects.get(user=consulta.data_aberta.user)
+
+        return render(request, 'consulta.html', {'consulta': consulta, 'dado_medico': dado_medico})
